087b050bd822ec36944771d35b90c6ea7ae76872
Malhar <malhar.deshpande18@gmail.com>
2017-09-13 17:11:03 +0530

    Blue Button Component

---
+++ Blue-Components/Blue-Primary-Button/blue-primary-button.component.ts
@@ -0,0 +1,77 @@
+class BluePrimaryButton extends HTMLElement {
+  private _disabled: boolean;
+  private _label: boolean;
+
+  constructor(){
+    super();
+    this.attachShadow({mode: 'open'});
+  }
+
+  get disabled(){
+    return this.hasAttribute('disabled');
+  }
+
+  set disabled(value){
+    const val = Boolean(value);
+    this._disabled = val;
+    if(val){
+      this.shadowRoot.querySelector('button').setAttribute('disabled', '');
+    } else {
+      this.shadowRoot.querySelector('button').removeAttribute('disabled');
+    }
+  }
+
+  get label(){
+    return this.getAttribute('label');
+  }
+
+  set label(value){
+    this._label = value;
+    this.shadowRoot.innerHTML = this.renderHTML(this._label);
+  }
+
+  static get observedAttributes(){
+    return ['disabled'];
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue){
+    this.disabled = newValue;
+  }
+
+  connectedCallback(){
+    this.shadowRoot.innerHTML = this.renderHTML(this._label);
+  }
+
+  renderHTML(lbl){
+    return `
+      <style>
+        :host .primary-button{
+          background-color: #243b4a;
+          font-size: 14px;
+          line-height: 1;
+          text-align: center;
+          color: #fff;
+          border: 0;
+          outline: 0;
+          height: 36px;
+        }
+        :host .primary-button:hover{
+          cursor: pointer;
+          color: #fff;
+        }
+        :host .primary-button[disabled]{
+          opacity: 0.96;
+          background-color: #fff;
+          border: 1px solid #d7d7d7;
+          font-size: 14px;
+          line-height: 1;
+          text-align: center;
+          color: #243b4a;
+        }
+      </style>
+      <button type="button" class="primary-button">${lbl}</button>
+    `;
+  }
+}
+
+customElements.define("b-primary-button", BluePrimaryButton);
